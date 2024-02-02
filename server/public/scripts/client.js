399affc052c361b006e9b319cfbffc0f33fe564c
Beth Tracy <bntracy@gmail.com>
2024-02-02 15:21:35 -0600

    GET and render working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,42 @@
-console.log('JS is sourced!');
+// console.log('JS is sourced!');
+
+function onReady() {
+    getItems();
+}
+
+onReady();
+
+function getItems() {
+    axios.get('/todos').then(response => {
+        // console.log(response.data);
+        renderItems(response.data);
+    }).catch(error => {
+        console.log('Error in GET', error);
+        alert('Something went wrong!');
+    });
+}
+
+function renderItems(items) {
+    document.getElementById('item_display').innerHTML = '';
+    for (let item of items) {
+        document.getElementById('item_display').innerHTML += `
+            <tr data-testid="toDoItem">
+                <td>${item.text}</td>
+                <td><button data-testid="completeButton" onclick="completeItem(${item.id})">Mark Done</button></td>
+                <td><button data-testid="deleteButton" onclick="deleteItem(${item.id})">Delete</button></td>
+            </tr>
+        `;
+    }
+}
+
+// function addItem(event) {
+//     event.preventDefault();
+//     let text = document.getElementById('to-do_input').value;
+//     axios.post('/todos', { text: text }).then(response => {
+//         getItems();
+//         document.getElementById('to-do_input').value = '';
+//     }).catch(error => {
+//         console.log('Error in POST', error);
+//         alert('Something went wrong!');
+//     });
+// }
--- server/routes/todos.router.js
+++ server/routes/todos.router.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
 const pool = require('../modules/pool');
 
-
+router.get('/', (req, res) => {
+    let queryText = 'SELECT * FROM "todos"';
+    pool.query(queryText).then(result => {
+        res.send(result.rows);
+    }).catch(err => {
+        console.log(`Error running query ${queryText}`, err);
+        res.sendStatus(500);
+    });
+});
 
 
 module.exports = router;
