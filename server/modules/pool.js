50a09574ac817e425b11ec82284ab98bee68fa46
Beth Tracy <bntracy@gmail.com>
2024-02-05 16:07:58 -0600

    update poo.js to use DATABASE_URL

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -6,11 +6,28 @@ if (process.env.NODE_ENV === 'test') {
   databaseName = 'prime_testing'
 }
 
-const pool = new pg.Pool({
+let pool;
+if (process.env.DATABSE_URL) {
+  pool = new pg.Pool({
+    connectionString: process.env.DATABSE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+} else {
+  pool = new pg.Pool({
     host: 'localhost',
     port: 5432,
     database: databaseName,
     allowExitOnIdle: true 
-})
+  });
+}
+
+// const pool = new pg.Pool({
+//     host: 'localhost',
+//     port: 5432,
+//     database: databaseName,
+//     allowExitOnIdle: true 
+// })
 
 module.exports = pool
